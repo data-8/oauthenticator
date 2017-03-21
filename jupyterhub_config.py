8d54bbcd42a5c4ae0ac09b188d774a2edbfb9fca
rzh <rzh@rzhs-MacBook-Pro.local>
2017-03-20 17:45:59 -0700

    got okpy authenticator working

---
+++ jupyterhub_config.py
@@ -0,0 +1,5 @@
+c.JupyterHub.spawner_class = 'simplespawner.SimpleLocalProcessSpawner'
+c.JupyterHub.authenticator_class = 'oauthenticator.OkpyOAuthenticator'
+c.OkpyOAuthenticator.client_id = "jupyterhub"
+c.OkpyOAuthenticator.client_secret = "BBjzDqxtQkMqqjp1n76CkcGYlO6qjme"
+
